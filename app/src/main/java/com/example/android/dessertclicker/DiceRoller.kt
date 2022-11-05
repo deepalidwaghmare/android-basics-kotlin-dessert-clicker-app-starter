041eb97d1b318eccba2abfb12588ec6e14360df8
Deepali <deepalidwaghmare@yahoo.com>
2022-11-05 10:05:44 +0000

    RollDice

--- app/src/main/java/com/example/android/dessertclicker/DiceRoller.kt
+++ app/src/main/java/com/example/android/dessertclicker/DiceRoller.kt
@@ -3,6 +3,7 @@ package com.example.android.dessertclicker
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,8 +14,22 @@ class DiceRoller: AppCompatActivity(){
 
         val rollButton: Button = findViewById(R.id.button1)
         rollButton.setOnClickListener {
+            rollDice()
+
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
         }
+    }
+    class Dice(val numSides:Int){
+        fun roll():Int{
+            return (1..numSides).random()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(12)
+        val diceRoll = dice.roll()
+        val resultTextView:TextView = findViewById(R.id.textView2)
+        resultTextView.text=diceRoll.toString()
     }
 }
