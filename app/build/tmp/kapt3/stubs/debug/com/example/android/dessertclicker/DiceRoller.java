39cc14f1e6d5d7cb24216426e1d67ddb266c2b1e
Deepali <deepalidwaghmare@yahoo.com>
2022-11-06 17:19:33 +0000

    RollDice

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/dessertclicker/DiceRoller.java
@@ -0,0 +1,36 @@
+package com.example.android.dessertclicker;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001:\u0001\bB\u0005\u00a2\u0006\u0002\u0010\u0002J\u0012\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u0006H\u0014J\b\u0010\u0007\u001a\u00020\u0004H\u0002\u00a8\u0006\t"}, d2 = {"Lcom/example/android/dessertclicker/DiceRoller;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "rollDice", "Dice", "app_debug"})
+public final class DiceRoller extends androidx.appcompat.app.AppCompatActivity {
+    
+    public DiceRoller() {
+        super();
+    }
+    
+    @java.lang.Override()
+    protected void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void rollDice() {
+    }
+    
+    @kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0002\b\u0005\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0006\u0010\u0007\u001a\u00020\u0003R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0005\u0010\u0006\u00a8\u0006\b"}, d2 = {"Lcom/example/android/dessertclicker/DiceRoller$Dice;", "", "numSides", "", "(I)V", "getNumSides", "()I", "roll", "app_debug"})
+    public static final class Dice {
+        private final int numSides = 0;
+        
+        public Dice(int numSides) {
+            super();
+        }
+        
+        public final int getNumSides() {
+            return 0;
+        }
+        
+        public final int roll() {
+            return 0;
+        }
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/example/android/dessertclicker/HappyBirthdayMessage.java
@@ -0,0 +1,17 @@
+package com.example.android.dessertclicker;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u001c\u0010\u0003\u001a\u00020\u00042\b\u0010\u0005\u001a\u0004\u0018\u00010\u00062\b\u0010\u0007\u001a\u0004\u0018\u00010\bH\u0016\u00a8\u0006\t"}, d2 = {"Lcom/example/android/dessertclicker/HappyBirthdayMessage;", "Landroidx/appcompat/app/AppCompatActivity;", "()V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "persistentState", "Landroid/os/PersistableBundle;", "app_debug"})
+public final class HappyBirthdayMessage extends androidx.appcompat.app.AppCompatActivity {
+    
+    public HappyBirthdayMessage() {
+        super();
+    }
+    
+    @java.lang.Override()
+    public void onCreate(@org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState, @org.jetbrains.annotations.Nullable()
+    android.os.PersistableBundle persistentState) {
+    }
+}
